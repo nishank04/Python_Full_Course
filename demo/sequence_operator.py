e5ff1a58afc967f8341cc9538e70cc529c398ee5
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-10-08 00:17:52 +0530

    Replacing fields, sliceback and sequence_operator

---
+++ demo/sequence_operator.py
@@ -0,0 +1,19 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pinig "
+string4 = "for the "
+string5 = "jordan"
+age = "32"
+print(string1 + string2 + string3 + string4 + string5)
+
+print("he's " " probably " "pining " "for the " "jordan")
+#print("Hello " + age)
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+
+today = "Thursday"
+print("day" in today)
+print("Thu" in today)
+print("fri" in today)
+print("3" in age)
---
+++ demo/sliceback.py
@@ -0,0 +1,17 @@
+letter = "abcdefghigklmnopqrstuvwxyz"
+backwards = letter[::-1]
+print(backwards)
+
+#create a slice that produce the character qpo
+print(letter[16:13:-1])
+
+#edcba
+print(letter[4::-1])
+
+#slice the string to produce the last 8 characters in reverse order
+print(letter[-1:-9:-1])
+print(letter[:-9:-1])
+print(letter[-4:])
+print(letter[-1:])
+print(letter[:1])
+print(letter[0])
