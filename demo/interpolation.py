8dd79785cb33ae89610adc341903ae4d42fe7554
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-10-12 00:11:44 +0530

    Interpolation, formatting and small quiz-12-10-2021

---
+++ demo/interpolation.py
@@ -0,0 +1,8 @@
+age = 32
+print("My age is "  + str(age) +" years")
+print("My age is %d years" % age)
+
+major = "Nishank"
+colonel = "Abhishek"
+print("My friend is %d %s, %d %s" % (age, major, 6, colonel))
+print("PI is approx %f" % (22/7))
